f035513ac86054644e79fad71519c626d73870c5
dbc201 <denizbcakiroglu@gmail.com>
2022-08-11 22:42:51 -0400

    cart stubs

--- routes/cart.js
+++ routes/cart.js
@@ -1,22 +1,77 @@
 const express = require("express");
 const path = require("path");
 
+const utilsInitializer = require(path.join(__dirname, "..", "utils", "initializeUtils.js"));
+
 const router = express.Router();
 
-router.get("/cart", function (req, res) {
-
+router.get("/cart", async function (req, res, next) {
+    try {
+        if (!req.session.loggedin) {
+            req.session.redirect = req.originalUrl;
+            res.status(403);
+            res.redirect("/account/login");
+        } else {
+            let rows = await utilsInitializer.cartUtils().getCart(req.session.account_id);
+            res.render("cart", {items: rows});
+        }
+    } catch (e) {
+        next(e);
+    }
 });
 
-router.post("/cart/add", function (req, res) {
+router.post("/cart/add", function (req, res, next) {
+    if (!req.session.loggedin) {
+        res.status(403);
+        res.send("not logged in");
+    } else {
+        let account_id = req.session.account_id;
+        let item_id = req.body.item_id;
 
+        if (!item_id) {
+            res.status(404);
+            return res.send("Bad Request");
+        }
+
+        utilsInitializer.cartUtils().addToCart(account_id, item_id).then(function () {
+            res.status(200);
+            res.send("OK");
+        }).catch(function (err) {
+            next(err);
+        });
+    }
 });
 
-router.post("/cart/remove", function (req, res) {
+router.post("/cart/remove", function (req, res, next) {
+    if (!req.session.loggedin) {
+        res.status(403);
+        res.send("not logged in");
+    } else {
+        let account_id = req.session.account_id;
+        let item_id = req.body.item_id;
 
+        if (!item_id) {
+            res.status(404);
+            return res.send("Bad Request");
+        }
+
+        utilsInitializer.cartUtils().removeFromCart(account_id, item_id).then(function () {
+            res.status(200);
+            res.send("OK");
+        }).catch(function (err) {
+            next(err);
+        });
+    }
 });
 
 router.post("/cart/purchase", function (req, res) {
-
+    if (!req.session.loggedin) {
+        res.status(403);
+        res.send("not logged in");
+    } else {
+        let account_id = req.session.account_id;
+        // TODO
+    }
 });
 
 module.exports = router;
